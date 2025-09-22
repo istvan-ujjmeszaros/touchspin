739a27f3c25408f55ab25e4f550dd242eec40617
Istvan Ujj-Meszaros <istvan@radaptor.com>
2025-09-22 15:06:50 +0200

    feat(jquery-plugin): add UMD bundles for each Bootstrap version
    This commit introduces new UMD bundles for the jQuery plugin, one for each Bootstrap version (3, 4, and 5). These bundles include the corresponding renderer, providing pre-built distributions for easier integration.
    
    The `package.json` file has been updated to:
    - Add new build scripts for creating the UMD bundles.
    - Include the Bootstrap renderers as direct dependencies.

---
+++ packages/jquery-plugin/src/jquery-touchspin-bs4.ts
@@ -0,0 +1,16 @@
+/**
+ * jQuery TouchSpin Bootstrap 4 UMD Bundle
+ *
+ * All-in-one bundle including:
+ * - jQuery plugin wrapper
+ * - Bootstrap 4 renderer
+ * - 100% backwards compatible with legacy TouchSpin
+ */
+
+import { installWithRenderer } from './index';
+
+// Import Bootstrap 4 renderer - this will be bundled into the UMD build
+import { Bootstrap4Renderer } from '@touchspin/renderer-bootstrap4';
+
+// Install the jQuery plugin with Bootstrap 4 renderer automatically
+installWithRenderer(Bootstrap4Renderer);
